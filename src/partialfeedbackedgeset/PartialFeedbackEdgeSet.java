1ae5bb6e02deb26fcf825ee6a939549e4b5f9b1b
John <johncerpa98@gmail.com>
2018-05-06 11:58:34 -0500

    Se puede aplicar una mejora para el I+D

--- src/partialfeedbackedgeset/PartialFeedbackEdgeSet.java
+++ src/partialfeedbackedgeset/PartialFeedbackEdgeSet.java
@@ -11,32 +11,71 @@ public class PartialFeedbackEdgeSet {
     public static ArrayList<ArrayList<Arista>> buscarSubgrafos(ArrayList<int[]> listaAdyacencia, int K) {        
         ArrayList<ArrayList<Arista>> listaSubgrafos = new ArrayList<>();
         
-        listaAdyacencia.forEach((a) -> {
+        int numVertice = 0;
+        
+        for (int[] vertice : listaAdyacencia) {
             ArrayList<Arista> subgrafo = new ArrayList<>();
-            for (int i = 0; i < a.length; i++) {
-                // Uniendo (1,0) ... (1,0) (1,4) ... (1,0) (1,4) (1,3)
-                subgrafo.add(new Arista(i, a.length));
-                listaSubgrafos.add(subgrafo);
-                
+            for (int i = 0; i < vertice.length; i++) {
+                // Agregando (1,0) ... (1,0) (1,4) ... (1,0) (1,4) (1,3)
+                subgrafo.add(new Arista(numVertice, vertice[i]));
+                ArrayList<Arista> temp = new ArrayList<>(subgrafo);
+                if (temp.size() == K) {
+                    listaSubgrafos.add(temp);
+                }
                 // Agregando (1,0) ... (1,4) ... (1,3)
                 if (i > 0) {
                     ArrayList<Arista> unElemento = new ArrayList<>();
-                    unElemento.add(new Arista(i, a[i]));
-                    listaSubgrafos.add(unElemento);
+                    unElemento.add(new Arista(numVertice, vertice[i]));
+                    if (unElemento.size() == K) {
+                        listaSubgrafos.add(unElemento);
+                    }                    
                 }                
-            }            
-        });
+            }   
+            numVertice++;
+        }
         
         // Verificar que subgrafos no se repitan, consumen mucho espacio.
+        removerRepetidos(listaSubgrafos);
         
         // Unir subgrafos
-        for (int i = 0; i < listaSubgrafos.size(); i++) {
-            for (int j = 0; j < listaSubgrafos.size(); j++) {
-                if (i != j) listaSubgrafos.get(i).addAll(listaSubgrafos.get(j));
-            }
+        int tam = listaSubgrafos.size();
+        for (int i = 0; i < tam; i++) {
+            for (int j = i + 1; j < tam - 1; j++) {
+                ArrayList<Arista> nuevaLista = new ArrayList<>(listaSubgrafos.get(i));
+                nuevaLista.addAll(listaSubgrafos.get(j));
+                listaSubgrafos.add(nuevaLista);
+            }                
         }       
                 
-        return listaSubgrafos;        
+        return listaSubgrafos;    
+    }
+    
+    public static ArrayList<ArrayList<Arista>> removerRepetidos(ArrayList<ArrayList<Arista>> lista) {
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).size() == 1) {
+                for (int j = i + 1; j < lista.size() - 1; j++) {
+                    if (lista.get(i).size() == 1) {
+                        int ui = lista.get(i).get(0).u;
+                        int vi = lista.get(i).get(0).v;
+                        
+                        int uj = lista.get(j).get(0).u;
+                        int vj = lista.get(j).get(0).v;
+                        if ((ui == uj && vi == vj) || (ui == vj && vi == uj)) {
+                            lista.remove(i);
+                        }
+                    }
+                }
+            } 
+        }
+        return lista;
+    }
+    
+    public static void mostrarLista(ArrayList<ArrayList<Arista>> subgrafos) {
+        for (ArrayList<Arista> lista : subgrafos) {
+            for (Arista a : lista)
+                System.out.print(a.u + "," + a.v + " ");
+            System.out.println("");
+        }
     }
 
     public static void main(String[] args) {
@@ -45,24 +84,20 @@ public class PartialFeedbackEdgeSet {
         
         ArrayList<int[]> grafo = new ArrayList<>();
         
-        int[] l0 = {1, 2};
-        int[] l1 = {0};
-        int[] l2 = {0};
-        //int[] l3 = {1, 4, 2};
-        //int[] l4 = {3, 0, 1};
+        int[] l0 = {1, 4};
+        int[] l1 = {0, 4, 3, 2};
+        int[] l2 = {1, 3};
+        int[] l3 = {1, 4, 2};
+        int[] l4 = {3, 0, 1};
         
         grafo.add(l0);
         grafo.add(l1);
         grafo.add(l2);
-        //grafo.add(l3);
-        //grafo.add(l4);
+        grafo.add(l3);
+        grafo.add(l4);
         
-        for (ArrayList<Arista> lista : buscarSubgrafos(grafo, 3)) {
-            for (Arista a : lista) {
-                System.out.print(a.u + "," + a.v + " ");
-            }
-            System.out.println("");
-        }
+        mostrarLista(buscarSubgrafos(grafo, 3));
+        //buscarCircuitos();
         
         System.out.println("Done!");
         
